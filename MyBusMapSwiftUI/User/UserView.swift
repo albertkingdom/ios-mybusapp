8efbba8d5a1d1ef3da004f64048a73048a66dc83
albertkingdom <albertkingdom@gmail.com>
2025-09-27 12:32:35 +0800

    Refactor UserView to integrate Perception tracking and improve layout structure

--- MyBusMapSwiftUI/User/UserView.swift
+++ MyBusMapSwiftUI/User/UserView.swift
@@ -11,6 +11,7 @@ import FirebaseCore
 import GoogleSignIn
 import GoogleSignInSwift
 import Kingfisher
+import Perception
 import SwiftUI
 
 @Reducer
@@ -94,62 +95,64 @@ struct UserView: View {
     let store: StoreOf<UserFeature>
 
     var body: some View {
-        VStack {
-            if store.imageUrl == nil {
-                Image(systemName: "person.fill")
-                    .resizable()
-                    //.border(.black, width: 1)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .clipShape(.circle)
-                    .shadow(radius: 3)
-                    .overlay {
-                        Circle().stroke(.gray, lineWidth: 3)
-                    }
+        WithPerceptionTracking {
+            VStack {
+                if store.imageUrl == nil {
+                    Image(systemName: "person.fill")
+                        .resizable()
+                        //.border(.black, width: 1)
+                        .frame(width: 100, height: 100, alignment: .center)
+                        .clipShape(.circle)
+                        .shadow(radius: 3)
+                        .overlay {
+                            Circle().stroke(.gray, lineWidth: 3)
+                        }
 
-            } else {
-                KFImage(store.imageUrl)
-                    .resizable()
-                    //.border(.black, width: 1)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .clipShape(.circle)
-                    .shadow(radius: 3)
-                    .overlay {
-                        Circle().stroke(.green, lineWidth: 3)
-                    }
+                } else {
+                    KFImage(store.imageUrl)
+                        .resizable()
+                        //.border(.black, width: 1)
+                        .frame(width: 100, height: 100, alignment: .center)
+                        .clipShape(.circle)
+                        .shadow(radius: 3)
+                        .overlay {
+                            Circle().stroke(.green, lineWidth: 3)
+                        }
 
-            }
-            Text(store.userEmail ?? "")
-            if let error = store.error {
-                Text(error)
-                    .foregroundColor(.red)
-                    .font(.caption)
-            }
-            if !store.isAuthenticated {
-                GoogleSignInButton(action: {
-                    Task {
-                        store.send(.signInWithGoogle)
-                    }
-                })
-                .frame(height: 50, alignment: .center)
-                .padding(.horizontal, 50)
-            }
-            Spacer()
-
-            if store.isAuthenticated {
-                Button {
-                    store.send(.signOut)
-                } label: {
-                    Text("登出".uppercased())
-                        .foregroundColor(Color.black)
-                        .fontWeight(.heavy)
                 }
-                .padding()
-                .frame(maxWidth: .infinity, alignment: .center)
+                Text(store.userEmail ?? "")
+                if let error = store.error {
+                    Text(error)
+                        .foregroundColor(.red)
+                        .font(.caption)
+                }
+                if !store.isAuthenticated {
+                    GoogleSignInButton(action: {
+                        Task {
+                            store.send(.signInWithGoogle)
+                        }
+                    })
+                    .frame(height: 50, alignment: .center)
+                    .padding(.horizontal, 50)
+                }
+                Spacer()
+
+                if store.isAuthenticated {
+                    Button {
+                        store.send(.signOut)
+                    } label: {
+                        Text("登出".uppercased())
+                            .foregroundColor(Color.black)
+                            .fontWeight(.heavy)
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity, alignment: .center)
+                }
             }
-        }
-        .padding([.top], 50)
-        .onAppear {
-            store.send(.checkAuthStatus)
+            .padding([.top], 50)
+            .onAppear {
+                store.send(.checkAuthStatus)
+            }
         }
     }
 
