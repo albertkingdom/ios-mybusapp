068014ae81be292efba8b6a176f09cfc89e8d504
albertkingdom <albertkingdom@gmail.com>
2026-01-25 12:36:42 +0800

    test: update mocks for auth session

--- MyBusMapTests/FavStationsViewModelTest.swift
+++ MyBusMapTests/FavStationsViewModelTest.swift
@@ -1,5 +1,5 @@
 //
-//  Test.swift
+//  FavStationsViewModelTest.swift
 //  MyBusMapTests
 //
 //  Created by yklin on 2024/11/2.
@@ -8,7 +8,6 @@
 import Realm
 import RealmSwift
 import Testing
-import FirebaseAuth
 
 @testable import MyBusMapSwiftUI
 
@@ -16,169 +15,145 @@ enum FirebaseError: Error {
     case getRemoteError
 }
 
-class MockFirebaseManagerService: FirebaseManagerProtocol {
-    var shouldReturnSuccess = false
+final class MockFirebaseManagerService: FirebaseManagerProtocol {
+    private let shouldReturnSuccess: Bool
     var mockFavorites: [Favorite] = [
         Favorite(name: "station1", stationID: "1"),
         Favorite(name: "station2", stationID: "2"),
     ]
+
     init(shouldReturnSuccess: Bool) {
         self.shouldReturnSuccess = shouldReturnSuccess
     }
+
     func getRemoteData(email: String) async -> [Favorite] {
-        if shouldReturnSuccess {
-            return mockFavorites
-        } else {
-            return []
-        }
+        shouldReturnSuccess ? mockFavorites : []
     }
 
-    func saveToRemote(email: String, favorite: MyBusMapSwiftUI.Favorite) {
+    func saveToRemote(email: String, favorite: Favorite) {}
 
-    }
-
-    func removeFromRemote(favorite: MyBusMapSwiftUI.Favorite) {
+    func removeFromRemote(favorite: Favorite) {
         mockFavorites = mockFavorites.filter { $0 != favorite }
     }
-
 }
 
-class MockRealmManger: RealmManagerProtocol {
+final class MockRealmManger: RealmManagerProtocol {
     var realm: Realm!
-    init() {
-        let config = Realm.Configuration(
-            inMemoryIdentifier: UUID().uuidString)
-        // Open the realm
-        do {
-            self.realm = try Realm(configuration: config)
-            do {
-                try realm?.write {
-                    self.realm.add(mockFavorites)
-                }
-            } catch let error {
-                print(error.localizedDescription)
-            }
-            
-            
-        } catch let error {
-            print(error.localizedDescription)
-        }
-    }
-    var mockFavorites: [FavoriteRealm] = [
+
+    private let mockFavorites: [FavoriteRealm] = [
         FavoriteRealm(name: "station1", stationID: "1"),
         FavoriteRealm(name: "station2", stationID: "2"),
     ]
-    func saveToDB(_ favorite: MyBusMapSwiftUI.FavoriteRealm) {
+
+    init() {
+        let config = Realm.Configuration(inMemoryIdentifier: UUID().uuidString)
         do {
-            try realm?.write {
-                realm?.add(favorite)
+            realm = try Realm(configuration: config)
+            try realm.write {
+                realm.add(mockFavorites)
             }
-        } catch let error {
+        } catch {
             print(error.localizedDescription)
         }
-       
     }
 
-    func readAllFromDB() -> RealmSwift.Results<MyBusMapSwiftUI.FavoriteRealm> {
-        
-            
-        let objects = realm.objects(FavoriteRealm.self)
-        return objects
-        
-        
+    func saveToDB(_ favorite: FavoriteRealm) {
+        do {
+            try realm.write {
+                realm.add(favorite)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 
-    func deleteFromDB(objectToDelete: MyBusMapSwiftUI.FavoriteRealm) {
+    func readAllFromDB() -> RealmSwift.Results<FavoriteRealm> {
+        realm.objects(FavoriteRealm.self)
+    }
+
+    func deleteFromDB(objectToDelete: FavoriteRealm) {
         do {
             try realm.write {
                 realm.delete(objectToDelete)
             }
-        } catch let error {
+        } catch {
             print(error.localizedDescription)
         }
     }
-            
-
 }
 
-final class MockFavStationsAuthManager: AuthManager {
-    override func checkIfLogin() -> FirebaseAuth.User? {
+final class MockFavStationsAuthManager: AuthManagerProtocol {
+    var isLogin = false
+    var email: String = ""
+
+    func checkIfLogin() -> UserSession? {
         isLogin = false
         email = ""
         return nil
     }
 
-    override func signIn(completion: @escaping (Result<AuthResult, Error>) -> Void) {
-    }
+    func signIn(completion: @escaping (Result<AuthResult, Error>) -> Void) {}
 
-    override func signOut() {
+    func signOut() {
         isLogin = false
         email = ""
     }
 }
-struct FavStationsViewModelTest {
 
+struct FavStationsViewModelTest {
     @Test func testGetRemoteData_success() async {
-        var viewModel: FavStationsViewModel = FavStationsViewModel(
-            firebaseService: MockFirebaseManagerService(
-                shouldReturnSuccess: true),
+        var viewModel = FavStationsViewModel(
+            firebaseService: MockFirebaseManagerService(shouldReturnSuccess: true),
             realmManager: MockRealmManger(),
-            authManager: MockFavStationsAuthManager())
+            authManager: MockFavStationsAuthManager()
+        )
         await viewModel.getRemoteData(email: "")
         #expect(viewModel.favoriteList.count == 2)
     }
 
     @Test func testGetRemoteData_failure() async {
-        var viewModel: FavStationsViewModel = FavStationsViewModel(
-            firebaseService: MockFirebaseManagerService(
-                shouldReturnSuccess: false),
+        var viewModel = FavStationsViewModel(
+            firebaseService: MockFirebaseManagerService(shouldReturnSuccess: false),
             realmManager: MockRealmManger(),
-            authManager: MockFavStationsAuthManager())
+            authManager: MockFavStationsAuthManager()
+        )
         await viewModel.getRemoteData(email: "")
         #expect(viewModel.favoriteList.count == 0)
     }
+
     @Test func testDeleteRemoteData() async {
-        let mockFirebaseService = MockFirebaseManagerService(
-            shouldReturnSuccess: true)
-        var viewModel: FavStationsViewModel = FavStationsViewModel(
+        let mockFirebaseService = MockFirebaseManagerService(shouldReturnSuccess: true)
+        var viewModel = FavStationsViewModel(
             firebaseService: mockFirebaseService,
             realmManager: MockRealmManger(),
-            authManager: MockFavStationsAuthManager())
+            authManager: MockFavStationsAuthManager()
+        )
         viewModel.favoriteList = mockFirebaseService.mockFavorites
         let preCount = viewModel.favoriteList.count
-        let favoriteToBeDeleted = mockFirebaseService.mockFavorites[0]
         viewModel.deleteRemoteData(indexSet: IndexSet(integer: 0))
         #expect(viewModel.favoriteList.count == preCount - 1)
     }
-    
-    
+
     @Test func testDeleteLocalData() async {
-        let mockFirebaseService = MockFirebaseManagerService(
-            shouldReturnSuccess: true)
         let mockRealmManager = MockRealmManger()
-        var viewModel: FavStationsViewModel = FavStationsViewModel(
-            firebaseService: mockFirebaseService,
+        var viewModel = FavStationsViewModel(
+            firebaseService: MockFirebaseManagerService(shouldReturnSuccess: true),
             realmManager: mockRealmManager,
-            authManager: MockFavStationsAuthManager())
-        
-//        viewModel.realmFavList = mockRealmManager.readAllFromDB()
-//        let preCount = viewModel.realmFavList.count
+            authManager: MockFavStationsAuthManager()
+        )
         viewModel.deleteLocalData(indexSet: IndexSet(integer: 0))
-        #expect(mockRealmManager.readAllFromDB().count==1)
+        #expect(mockRealmManager.readAllFromDB().count == 1)
         #expect(viewModel.realmFavList.count == 1)
-       
     }
-    
+
     @Test func testReadLocalData() {
-        let mockFirebaseService = MockFirebaseManagerService(
-            shouldReturnSuccess: true)
-        let mockRealmManager = MockRealmManger()
-        let viewModel: FavStationsViewModel = FavStationsViewModel(
-            firebaseService: mockFirebaseService,
-            realmManager: mockRealmManager,
-            authManager: MockFavStationsAuthManager())
-        
+        let viewModel = FavStationsViewModel(
+            firebaseService: MockFirebaseManagerService(shouldReturnSuccess: true),
+            realmManager: MockRealmManger(),
+            authManager: MockFavStationsAuthManager()
+        )
         viewModel.readLocalData()
         #expect(viewModel.realmFavList.count == 2)
     }
 }
+
